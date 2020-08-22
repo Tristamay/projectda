c5801fc67b03d204de30196ac4dbfc449146f5e5
Conor Quinlan <conorquinlan@icloud.com>
2020-08-22 02:50:38 -0700

    Testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-// Git Testing.
 
-// Test 3
 void main() {
   runApp(MyApp());
 }
@@ -11,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ProjectDA',
       theme: ThemeData(
         // This is the theme of your application.
         //
